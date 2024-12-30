b0bf69d6ae6716291f9c0be2df12c3948d8527ab
VINOTHKUMARM1127 <vinothmaddy061011@gmail.com>
2024-12-30 23:44:10 +0530

    minor changes

--- src/components/HomePage/GamesList/GameList.jsx
+++ src/components/HomePage/GamesList/GameList.jsx
@@ -1,13 +1,11 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import "./GameList.css";
 import FetchData from '../../../FetchData/FetchData';
-import { FaArrowRight } from "react-icons/fa";
-import { FaArrowLeft } from "react-icons/fa";
 
 const GameList = ({dat, title}) => {
-  const listRef = useRef(null)
+
   const { data, loading } = FetchData(dat);
-  const [more, setMore] = useState(20)
+  const [more, setMore] = useState(10)
   console.log(title,data)
   if (loading) {
     return <div>Loading...</div>;
@@ -21,30 +19,10 @@ const GameList = ({dat, title}) => {
     setMore((prev) => prev + 10)
   }
 
-  const ScrollLeft = () => {
-    listRef.current.scrollBy({
-      top: 0,
-      left: -790,
-      behavior: "smooth",
-    })
-  }
-
-  const ScrollRight = () => {
-    listRef.current.scrollBy({
-      top: 0,
-      left: 790,
-      behavior: "smooth",
-    })
-  }
-
   return (
     <section>
       <div className="title">{title}</div>
-      <div className='icon-box'>
-        <FaArrowLeft className='icon left' onClick={()=>{ScrollLeft()}} />
-        <FaArrowRight className='icon right'onClick={()=>{ScrollRight()}} />
-      </div>
-      <div className="card-sec" ref={listRef}>
+      <div className="card-sec" >
         {data.slice(0, more).map((item) => {
           return (
             <div className="card" key={item.id}>
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -9,10 +9,7 @@ const HomePage = () => {
   return (
     <>
     <GameList dat={"/games?platform=pc"}  title={"PC Games"} />
-    <GameList dat={"/games?platform=browser"}  title={"Browser Games"} />
-    <GameList dat={"/games?category=battle-royale"}  title={"Battle Royal Games"} />
-    <GameList dat={"/games?category=racing"}  title={"Racing Games"} />
-    <GameList dat={"/games?category=shooter"}  title={"Shooting Games"} />
+    
     </>
   )
 }
