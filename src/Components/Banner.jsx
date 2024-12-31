1620da65959f5bc8050476891ddca1c96220f81d
VINOTHKUMARM1127 <vinothmaddy061011@gmail.com>
2025-01-01 00:20:12 +0530

    minor changes

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 
-const Banner = ({datas}) => {
-    
+const Banner = ({ datas }) => {
+
     const final = Math.floor(Math.random() * 20)
 
     return (
         <div>
-            {datas.slice(final - 1, final).map((item,index) => (
+            {datas.slice(final - 1, final).map((item, index) => (
                 <div key={index} className='relative'>
                     <div className='text-white font-bold text-[1.5em] absolute bottom-0 p-5
                         bg-gradient-to-t from-slate-900 to-transparent w-full rounded-2xl'>
                         {item.name}</div>
-                    <img src={item.background_image} alt=""
-                        className='md:h-[350px] w-full object-cover rounded-2xl' />
+                    <img src={item.background_image}
+                        alt={item.name}
+                        className=' md:h-[350px] w-full object-cover rounded=tl-2xl rounded=tr-2xl' />
                 </div>
             ))}
         </div>
