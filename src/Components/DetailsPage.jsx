5d4c0ed2886abd9f6826097c6dd1faa8caea94b8
VINOTHKUMARM1127 <vinothmaddy061011@gmail.com>
2025-01-06 15:01:25 +0530

    add Details page

--- src/Components/DetailsPage.jsx
+++ src/Components/DetailsPage.jsx
@@ -1,20 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import FetchData from '../FetchData/FetchData';
+import GamesGenre from './GamesGenre';
 
 const DetailsPage = () => {
-  const {id} = useParams();
+  const { id } = useParams();
+  const [expand, setExpand] = useState(false)
   const { data, loading } = FetchData({ url: `/games/${id}`, id: null })
-  console.log('Details',data)
+  console.log('Details', data)
   if (loading) {
     return (<div>Loading...</div>)
-}
+  }
+
+  const finalDes = data.description_raw.slice(0, data.description_raw.indexOf('Español'))
+
+  const cutDes = (des) => {
+    return des?.split(" ").slice(0, 50).join(" ") + (des?.split(" ").length > 50 ? "..." : "");
+  };
+
+  const formatDate = (dateString) => {
+    const options = { year: "numeric", month: "short", day: "numeric" };
+    return new Date(dateString).toLocaleDateString("en-US", options);
+  };
 
   return (
-    <div>
-      <img className='w-[90%] mx-auto my-4 ' src={data.background_image} alt={data.name} />
-      <div>{data.name}</div>
-    </div>
+    <>
+      <div className='text-white flex flex-col md:flex-row md:mt-[4em] w-[90%] mx-[auto] my-[2em]'>
+        <img className='w-[90%] md:max-w-[320px] md:h-[450px] object-cover mx-auto my-0em md:mx-[0em] rounded-xl '
+          src={data.background_image} alt={data.name} />
+
+        <div className='md:ml-[3em]'>
+          <div className='text-[1.5em] md:text-[2em] mt-[0.5em] md:mt-[0em] font-bold'>{data.name}</div>
+          <div className='text-[1.1em] opacity-90'>{data.genres[0].name}</div>
+          <div>
+            <div className='font-bold text-[1.3em] mb-[0.2em]'>Description</div>
+            <div className=" text-[1em] text-justify">
+              {expand ? finalDes : cutDes(finalDes)}
+            </div>
+            <button className="text-blue-200" onClick={() => setExpand(!expand)}>
+              {expand ? "Show less" : "Show more"}
+            </button>
+          </div>
+
+          <div className='flex my-[0.5em]'>
+            <div className='font-bold text-[1.1em]'>Developed By:</div>
+            <div className=' text-[1em] ml-[1em] opacity-90'>{data.developers[0].name}</div>
+          </div> <hr />
+
+          <div className='flex my-[0.5em]'>
+            <div className='font-bold text-[1.1em]'>Platforms:</div>
+            <div className='flex flex-wrap text-[1em] ml-[1em] opacity-90'>
+              {data.platforms.map((it, index) => (
+                <div className='w-120px'>
+                  {data.platforms.length > 1 && index !== data.platforms.length - 1 ?
+                    `${it.platform.name} ,` :
+                    it.platform.name}</div>
+              ))}</div>
+          </div> <hr />
+
+          <div className='flex my-[0.5em]'>
+            <div className='font-bold text-[1.1em]'>Released On:</div>
+            <div className=' text-[1em] ml-[1em] opacity-90'>{formatDate(data.released)}</div>
+          </div> <hr />
+
+          <div className='flex my-[0.5em]'>
+            <div className='font-bold text-[1.1em]'>Stores:</div>
+            <div className='flex flex-wrap text-[1em] ml-[1em] opacity-90'>
+              {data.stores.map((it, index) => (
+                <div className='w-120px'>
+                  {data.stores.length > 1 && index !== data.stores.length - 1 ?
+                    `${it.store.name} ,` :
+                    it.store.name}</div>
+              ))}</div>
+          </div> <hr />
+
+        </div>
+        <GamesGenre />
+      </div>
+    </>
+
   )
 }
 
--- src/Components/Trending.jsx
+++ src/Components/Trending.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Trending = ({ datas }) => {
+  const navigate = useNavigate();
 
   return (
     <div className='mt-5 hidden md:block'>
       <div className='text-white text-[30px] font-bold'>Trending Games</div>
       <div className='hidden md:grid md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4'>
         {datas.map((cur, index) => index < 4 && (
-          <div key={index} className=' bg-slate-900  rounded-xl
+          <div onClick={() => navigate(`/games/${cur.id}`)}
+           key={index} className=' bg-slate-900  rounded-xl
         group hover:scale-110 transtion-all duration-300 ease-in-out'>
             <img src={cur.background_image} alt={cur.name} className='rounded-tl-xl rounded-tr-xl
             object-cover h-[270px]'/>
