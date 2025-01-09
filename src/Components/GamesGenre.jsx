c5eee2ecedcbe45cb26677f35d04236919a8324a
VINOTHKUMARM1127 <vinothmaddy061011@gmail.com>
2025-01-09 11:29:48 +0530

    minor changes

--- src/Components/GamesGenre.jsx
+++ src/Components/GamesGenre.jsx
@@ -6,12 +6,34 @@ const GamesGenre = ({ genreId }) => {
     const { data, loading } = FetchData({ url: '/games', id: genreId })
     const dat = data?.results
     const navigate = useNavigate();
-    
+
+    const sk = () => {
+        return(
+            <div className='pb-1 text-center'>
+                <div className='bg-[#09152b] animate-pulse w-full h-[200px] md:h-[180px] 
+                rounded-tl-xl rounded-tr-xl object-cover mb-1'></div>
+                <div className='bg-[#09152b] animate-pulse  text-[1em] text-center 
+                pt-3 px-5 pb-3 mb-1'></div>
+                <div className='bg-[#09152b] animate-pulse  text-[1em] text-center 
+                pt-3 px-5 pb-3 rounded-bl-xl rounded-br-xl mb-1'></div>
+            </div>
+        )
+    }    
     return (
-        <>
-        {loading ? ('popular') :(
+        <><div className=' text-[30px] font-bold'>Popular Games</div>
+        {loading ? (
+            <div className='grid grid-cols-2 lg:grid-cols-3 gap-6 mt-3'>
+                {sk()}
+                {sk()}
+                {sk()}
+                {sk()}
+                {sk()}
+                {sk()}
+                {sk()}
+                {sk()}
+            </div>
+        ) :(
         <div className='mt-5'>
-            <div className=' text-[30px] font-bold'>Popular Games</div>
             <div className='grid grid-cols-2 lg:grid-cols-3 gap-6 mt-3'>
                 {dat.map((curitem, index) => (
                     <div key={index}
