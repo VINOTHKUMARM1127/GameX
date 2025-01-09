fa2a9a375790cf154cb31513d8782d97d395233e
VINOTHKUMARM1127 <vinothmaddy061011@gmail.com>
2025-01-09 14:46:19 +0530

    adding Menu

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { IoGameController } from "react-icons/io5";
 import { IoMenu } from "react-icons/io5";
+import { IoMdClose } from "react-icons/io";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -8,11 +9,10 @@ const Header = () => {
   return (
     <div className='flex p-[1em] bg-[#09152b] justify-between'>
       <div className='flex text-blue-400'>
-        <IoGameController className='mx-0px my-auto w-6 h-6 md:w-10 h-10' />
+        <IoGameController className='mx-0px my-auto w-6 h-6 md:w-10 md:h-10' />
         <div className='text-xl font-bold mx-0px my-auto'>LOTGAMES</div>
       </div>
-      <div className='text-[0.9em] text-blue-400 font-bold md:flex'>
-        {/* Desktop Menu */}
+      <div className='text-[1em] text-blue-400 font-bold md:flex mx-0px my-auto'>
         <div className='hidden md:block mr-[4em]'>HOME</div>
         <div className='hidden md:block mr-[4em]'>STORE</div>
         <div className='hidden md:block mr-[4em]'>CREATOR</div>
@@ -24,12 +24,15 @@ const Header = () => {
       </div>
 
       <div
-        className={`z-40 md:hidden ${menuOpen ? 'block' : 'hidden'} absolute top-[60px] left-0 w-full bg-[#09152b] p-4 rounded-lg`}
-      >
-        <div className='text-[.9em] mb-[4em] text-center text-blue-400 font-bold'>HOME</div>
-        <div className='text-[.9em] mb-[4em] text-center text-blue-400 font-bold'>STORE</div>
-        <div className='text-[.9em] mb-[4em] text-center text-blue-400 font-bold'>CREATOR</div>
-        <div className='text-[.9em] mb-[4em] text-center text-blue-400 font-bold'>ABOUT US</div>
+        className={`text-center text-blue-400 font-bold z-40 md:hidden 
+        ${menuOpen ? 'block' : 'hidden'} text-[.9em]
+        fixed top-0 right-0 w-[60vw] h-screen bg-[#09152b] pt-[4em] p-4 rounded-lg`}>
+        <IoMdClose className='w-6 h-6 top-3 absolute' 
+        onClick={() => {setMenuOpen(false)}}/>
+        <div className='mb-[4em]'>HOME</div>
+        <div className='mb-[4em]'>STORE</div>
+        <div className='mb-[4em]'>CREATOR</div>
+        <div className='mb-[4em]'>ABOUT US</div>
       </div>
     </div>
   );
